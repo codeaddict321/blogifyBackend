0041699fb2809d473b08b034581929fd9391e514
Rishad Ali <technicalboyrishad@gmail.com>
2023-08-11 12:47:08 +0530

    destructured req.body

--- controllers/blogControllers/writeBlog.js
+++ controllers/blogControllers/writeBlog.js
@@ -1,9 +1,9 @@
 const Blog = require('../../model/Blog')
 
 const controller = async (req,res)=>{
-   const {title,content}  = req.body;
+   const { author,timestamp,content}  = req.body;
   try{
-   await Blog.create({title,content});// Using the correct method for inserting
+   await Blog.create({author,timestamp,content});// Using the correct method for inserting
    res.json({ 'msg': "blog created" });
   } catch(err){
    res.status(500).json({ 'error': "Failed to create blog" });
