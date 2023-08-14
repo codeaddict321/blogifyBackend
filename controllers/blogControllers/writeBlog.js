6bfbb1ee8c4bbeb9a73b8aa753fb20c02fdb87ec
Rishad Ali <technicalboyrishad@gmail.com>
2023-08-14 19:15:11 +0530

    added author,timestamp,title

--- controllers/blogControllers/writeBlog.js
+++ controllers/blogControllers/writeBlog.js
@@ -1,9 +1,10 @@
 const Blog = require('../../model/Blog')
 
 const controller = async (req,res)=>{
-   const { author,timestamp,content}  = req.body;
+   const { author,content,title}  = req.body;
+  
   try{
-   await Blog.create({author,timestamp,content});// Using the correct method for inserting
+   await Blog.create({author,content,title});// Using the correct method for inserting
    res.json({ 'msg': "blog created" });
   } catch(err){
    res.status(500).json({ 'error': "Failed to create blog" });
--- model/Blog.js
+++ model/Blog.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
+
 const {Schema} = mongoose;
 const {model} = mongoose
 const blogSchema = new Schema({
     title:String,
     content:String,
+    createdAt: {
+        type: Date,
+        default: Date.now // Set default value to the current date and time
+    },
+    author:String
     
 })
 
